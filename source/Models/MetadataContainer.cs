14d9aaeef5b8eaf877127036d272f948a31a4f36
sammasamvi <tfsmasamvi@gmail.com>
2015-04-21 12:24:52 +0100

    takks

---
+++ source/Models/MetadataContainer.cs
@@ -0,0 +1,87 @@
+﻿using IUnification.Models.Enums;
+using IUnification.Models.Interfaces;
+using System;
+
+namespace LocalDriveUnificationPlugin.Models
+{
+    internal sealed class MetadataContainer : IMetadataContainer
+    {
+        private string          _Album;
+        private string          _Artist;
+        private string          _StringDuration;
+        private readonly string _FilePath;
+        private TimeSpan        _TimeSpanDuration;
+        private static string[] _MetadataFields = new string[] 
+        {
+            "Album",
+            "Artist",
+            "Duration",
+            "Title"
+        };
+        private string          _Title;
+
+        public Uri Datasource
+        {
+            get 
+            { 
+                return new Uri(_FilePath, UriKind.Absolute); 
+            }
+        }
+
+        public DatasourceFormat DatasourceFormat
+        {
+            get 
+            {
+                return DatasourceFormat.Local;
+            }
+        }
+
+        public TimeSpan Duration
+        {
+            get 
+            {
+                return _TimeSpanDuration;
+            }
+        }
+
+        private void GenerateMetadata()
+        {
+
+        }
+
+        public string Metadata(string MetadataField)
+        {
+            switch (MetadataField)
+            {
+                case "Album":
+                    return _Album;
+
+                case "Artist":
+                    return _Artist;
+
+                case "Duration":
+                    return _StringDuration;
+
+                case "Title":
+                    return _Title;
+
+                default:
+                    return "";
+            }
+        }
+
+        public MetadataContainer(String FilePath)
+        {
+            _FilePath = FilePath;
+            GenerateMetadata();
+        }
+
+        public string[] MetadataFields
+        {
+            get 
+            {
+                return MetadataContainer._MetadataFields;
+            }
+        }
+    }
+}
---
+++ source/Models/Plugin.cs
@@ -0,0 +1,114 @@
+﻿using DirectoryScanner.Common;
+using DirectoryScanner.Synchronous;
+using IUnification.Models;
+using IUnification.Models.Enums;
+using IUnification.Models.Interfaces;
+using System;
+using System.ComponentModel;
+
+namespace LocalDriveUnificationPlugin.Models
+{
+    public sealed class Plugin : IUnificationPlugin, IConfigurableUnificationPlugin
+    {
+        BindingList<IMetadataContainer> _MetadataCollection;
+        InputField[]                    _InputFields;
+
+        public string Author
+        {
+            get 
+            { 
+                return @"snwmelt"; 
+            }
+        }
+
+        public void Dispose()
+        {
+            MetadataCollection.Clear();
+        }
+
+        public void CreateInputFields()
+        {
+            InputFields[0] = new InputField(@"Scan Directory", FieldType.Standard);
+            InputFields[1] = new InputField(@"File Extensions", FieldType.Standard);
+        }
+
+        public InputField[] InputFields
+        {
+            set
+            {
+                _InputFields = value;
+            }
+
+            get
+            {
+                return _InputFields;
+            }
+        }
+
+        public BindingList<IMetadataContainer> MetadataCollection
+        {
+            private set
+            {
+                _MetadataCollection = value;
+            }
+
+            get 
+            { 
+                return _MetadataCollection; 
+            }
+        }
+
+        public event EventHandler<LoadingCompletedEventArgs> MetadataCollectionUpdatedEvent;
+
+        public Plugin()
+        {
+            InputFields        = new InputField[2];
+            MetadataCollection = new BindingList<IMetadataContainer>();
+            
+            CreateInputFields();
+        }
+
+        public PropertyContainer[] Properties
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Title
+        {
+            get 
+            { 
+                return @"Local Drive Plugin"; 
+            }
+        }
+
+        public void UpdateMetadataCollection()
+        {
+            Scanner _Scanner = new Scanner(InputFields[0].Value, true, ScanMode.MatchExtension, InputFields[1].Value.Split(','));
+            _Scanner.FileFoundEvent += ((sender, FileInfo) => 
+            {
+                IMetadataContainer NewMetadataContainer = new MetadataContainer(FileInfo.FullName);
+                MetadataCollection.Add(NewMetadataContainer);
+            });
+
+            _Scanner.Scan();
+
+            if (MetadataCollectionUpdatedEvent != null)
+                MetadataCollectionUpdatedEvent(this, new LoadingCompletedEventArgs(LoadingState.Complete));
+        }
+
+        public string Version
+        {
+            get 
+            { 
+                return @"0.0.1"; 
+            }
+        }
+    }
+}
